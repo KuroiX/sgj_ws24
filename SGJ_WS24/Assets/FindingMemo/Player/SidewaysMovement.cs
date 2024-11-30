7d38b079097d062b349007c4462df217b5498b21
Kerstin Laptop <kerstin@irox-games.com>
2024-11-30 12:53:36 +0100

    add side limits to player

--- SGJ_WS24/Assets/FindingMemo/Player/SidewaysMovement.cs
+++ SGJ_WS24/Assets/FindingMemo/Player/SidewaysMovement.cs
@@ -4,18 +4,25 @@ using UnityEngine.InputSystem;
 
 namespace FindingMemo.Player
 {
-    public class SidewaysMovement : MonoBehaviour
+    [ExecuteAlways] public class SidewaysMovement : MonoBehaviour
     {
         [SerializeField] private float accelerationPerFrame = .1f;
         [SerializeField] private float maxSpeed = 10;
         [SerializeField] private bool doAccelerate = true;
+        [SerializeField] private float sideLimit = 8.5f;
 
         private Controls actions;
 
         private bool isMoving = false;
         private float currentSpeed = 0;
         private int sign = 0;
+        private Vector3 initialPosition;
 
+
+        private void Awake()
+        {
+            initialPosition = transform.position;
+        }
 
         private void OnEnable()
         {
@@ -30,6 +37,13 @@ namespace FindingMemo.Player
             actions?.Default.Disable();
         }
 
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawSphere(initialPosition + Vector3.right * sideLimit, .1f);
+            Gizmos.DrawSphere(initialPosition + Vector3.left * sideLimit, .1f);
+        }
+#endif
 
         private void OnMoveInput(InputAction.CallbackContext input)
         {
@@ -48,7 +62,12 @@ namespace FindingMemo.Player
             currentSpeed = doAccelerate
                 ? Math.Clamp(currentSpeed + accelerationPerFrame, 0, maxSpeed)
                 : maxSpeed;
+
             transform.position += sign * currentSpeed * Time.deltaTime * Vector3.right;
+            transform.position = new Vector3(
+                Mathf.Clamp(transform.position.x, -sideLimit, sideLimit),
+                transform.position.y,
+                transform.position.z);
         }
 
         private void OnCancelMoveInput(InputAction.CallbackContext input)
