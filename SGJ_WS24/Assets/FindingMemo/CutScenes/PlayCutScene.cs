8c442dfdfba955ef310f3647d439960e0042422c
Kerstin Laptop <kerstin@irox-games.com>
2024-12-01 11:13:01 +0100

    add cut scenes with voice lines

---
+++ SGJ_WS24/Assets/FindingMemo/CutScenes/PlayCutScene.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace FindingMemo.CutScenes
+{
+    public class PlayCutScene : MonoBehaviour
+    {
+        [SerializeField] private bool playOnAwake = true;
+        [SerializeField] private SceneLoader sceneLoader;
+
+        private Animator animator;
+        private static readonly int Start = Animator.StringToHash("Start");
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            if (playOnAwake) StartCutScene();
+        }
+
+        [ContextMenu("Start Cut Scene")]
+        public void StartCutScene()
+        {
+            animator.SetTrigger(Start);
+        }
+
+
+        private void OnVideoFinishedPlaying()
+        {
+            print("video finished playing");
+            sceneLoader.LoadNextScene();
+        }
+    }
+}
---
+++ SGJ_WS24/Assets/FindingMemo/CutScenes/VoiceLineTriggerer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FMODUnity;
+using UnityEngine;
+
+namespace FindingMemo.CutScenes
+{
+    public class VoiceLineTriggerer : MonoBehaviour
+    {
+        [SerializeField] private EventReference[] voiceLines;
+
+        private int counter = 0;
+
+        public void PlayNextLine()
+        {
+            if (counter == voiceLines.Length) return;
+
+            var instance = RuntimeManager.CreateInstance(voiceLines[counter]);
+            instance.start();
+            counter++;
+        }
+    }
+}
