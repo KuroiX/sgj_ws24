d41b091a01f59acd250c85ddad2a56b2a3ca4cf4
Julian Geheeb <julian.geheeb+github@gmail.com>
2024-12-01 09:06:53 +0100

    Added gamestate

--- SGJ_WS24/Assets/GameManager.cs
+++ SGJ_WS24/Assets/GameManager.cs
@@ -1,19 +1,63 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using FindingMemo.Background;
+using FindingMemo;
 using FindingMemo.Neurons;
+using FindingMemo.Player;
 using FMODUnity;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public event Action<GameState> OnGameStateChanged;
+
+    [SerializeField] private HitNeurons hitNeurons;
+
+    public GameState GameState
+    {
+        get => _gameState;
+        set
+        {
+            _gameState = value;
+            OnGameStateChanged?.Invoke(_gameState);
+        }
+    }
+    
+    private GameState _gameState = GameState.Waiting;
+    
     [SerializeField]
     private EventReference music;
     public FMOD.Studio.EventInstance musicPlayEvent;
     public List<MoveDown> moveDown;
 
+    private void OnEnable()
+    {
+        foreach (var down in moveDown)
+        {
+            BeatTrackerr.fixedBeatUpdate += down.StartScrolling;
+        }
+        
+        hitNeurons.OnHitted += OnHitted;
+    }
+
+    private void OnHitted()
+    {
+        if (_gameState != GameState.Waiting) return;
+        
+        GameState = GameState.Playing;
+        StartGame();
+    }
+
+    private void OnDisable()
+    {
+        foreach (var down in moveDown)
+        {
+            BeatTrackerr.fixedBeatUpdate -= down.StartScrolling;
+        }
+        
+        hitNeurons.OnHitted -= OnHitted;
+    }
+
+    /*
     private void Start()
     {
         FMOD.Studio.EventDescription des;
@@ -38,6 +82,7 @@ public class GameManager : MonoBehaviour
             StartGame();
         }
     }
+    */
 
 
     [ContextMenu("Start scrolling")]
